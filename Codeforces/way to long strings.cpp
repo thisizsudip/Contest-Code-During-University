025e0bcc2d473590b38cf6f48f3ea44880e55b5c
Sudip <sudip.paul.16.3.2005@gmail.com>
2026-02-12 01:00:57 +0600

    way too long strings

---
+++ Codeforces/way to long strings.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    while(n--){
+        string s;
+        cin >> s;
+        int len;
+        len = s.length();
+        if(len > 10){
+            cout << s[0] << len - 2 << s[len - 1] << endl;
+        }
+        else{
+            cout << s << endl;
+        }
+
+    }
+}
