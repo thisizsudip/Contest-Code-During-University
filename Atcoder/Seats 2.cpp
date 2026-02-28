70718f229e63a9cded31b8ce10880a08730f366d
Sudip <sudip.paul.16.3.2005@gmail.com>
2026-02-28 19:43:00 +0600

    Seats 2.cpp

---
+++ Atcoder/Seats 2.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    if((2 *n) - 1 >= m){
+        cout << "YES\n";
+    }
+    else{
+        cout << "NO\n";
+    }
+}
